f22d8a37d4d31cfb3fb4fbcc62a39609c6145499
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-31 07:45:16 +0300

    adding event listener to the form

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-const h6 = document.createElement("h6");
-h6.textContent = "copyrightsreserved@MildredChepkoech";
-document.querySelector("body").appendChild(h6);
+// const h6 = document.createElement("h6");
+// h6.textContent = "copyrightsreserved@MildredChepkoech";
+// document.querySelector("body").appendChild(h6);
+
 
     fetch("https://random-d.uk/api/random")//, {
      //mode: 'cors'   
@@ -14,3 +15,43 @@ document.querySelector("body").appendChild(h6);
     cardImage.src = data.url
   } 
 })
+ 
+
+const likeCount = document.querySelector("#like-count")
+let likes;
+
+
+
+const likesbutton = document.querySelector('#like-button').addEventListener('click', ()=>{
+    likes += 1
+    addLikes()
+})
+
+const commentsList = document.querySelector('#comments-list')
+let  form = document.querySelector('form')
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    showComment(e.target.comment.value)
+    form.reset()
+})
+
+
+
+
+function showComment(comment) {
+    console.log(comment);
+    const p  = document.createElement("p");
+    let btn = document.createElement('button')
+    btn.addEventListener('click', deleteHandle)
+      btn.textContent = 'delete'
+      p.textContent = `${comment}`
+    p.appendChild(btn)
+    commentsList.append(p);
+  }
+  
+   function deleteHandle(e){
+      e.target.parentNode.remove()
+   }
+  function addLikes() {
+    likeCount.textContent = `${likes} likes`;
+  }
